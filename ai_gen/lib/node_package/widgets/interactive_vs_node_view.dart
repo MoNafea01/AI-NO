d36754eb53224a63f95fba870caea3880ffb2708
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-03-07 13:36:28 +0200

    ignore .pyc files

--- ai_gen/lib/node_package/widgets/interactive_vs_node_view.dart
+++ ai_gen/lib/node_package/widgets/interactive_vs_node_view.dart
@@ -1,12 +1,12 @@
 import 'package:ai_gen/node_package/data/vs_node_data_provider.dart';
 import 'package:ai_gen/node_package/widgets/GridCubit/grid_cubit.dart';
-import 'package:ai_gen/node_package/widgets/GridPainter/grid_painter.dart';
 import 'package:ai_gen/node_package/widgets/Resuable%20Widgets/custom_button.dart';
 import 'package:ai_gen/node_package/widgets/vs_node_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'CustomWIdgets/floating_buttons.dart';
+import 'GridPainter/grid_painter.dart';
 
 class InteractiveVSNodeView extends StatelessWidget {
   final double width;
@@ -86,23 +86,27 @@ class _InteractiveVSNodeViewBodyState
       builder: (context, state) {
         return Stack(
           children: [
-            if (state.showGrid)
-              CustomPaint(
-                size: Size(widget.width, widget.height),
-                painter: GridPainter(
-                    gridSpacing: state.gridSpacing, gridColor: state.gridColor),
-              ),
             InteractiveViewer(
               maxScale: 5.0,
               minScale: 0.2,
               panEnabled: true,
               scaleEnabled: true,
               constrained: false,
-              transformationController: _transformationController,
+              // transformationController: _transformationController,
               child: SizedBox(
                 width: widget.width,
                 height: widget.height,
-                child: VSNodeView(nodeDataProvider: widget.nodeDataProvider),
+                child: Stack(children: [
+                  if (state.showGrid)
+                    CustomPaint(
+                      size: Size(widget.width, widget.height),
+                      painter: GridPainter(
+                        gridSpacing: 30,
+                        gridColor: state.gridColor,
+                      ),
+                    ),
+                  VSNodeView(nodeDataProvider: widget.nodeDataProvider),
+                ]),
               ),
             ),
             Positioned(
@@ -117,16 +121,17 @@ class _InteractiveVSNodeViewBodyState
                 width: 57,
                 padding: const EdgeInsets.symmetric(vertical: 6),
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: const Color(0xffE6E6E6)),
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(0xffE6E6E6),
+                ),
                 child: Column(
                   children: [
                     CustomButton(
                         onTap: () {
-                          double newZoom = (state.zoomLevel + 5).clamp(5, 500);
-                          context.read<GridCubit>().updateZoom(newZoom);
-                          _transformationController.value = Matrix4.identity()
-                            ..scale(newZoom / 100);
+                          // double newZoom = (state.zoomLevel + 5).clamp(5, 500);
+                          // context.read<GridCubit>().updateZoom(newZoom);
+                          // _transformationController.value = Matrix4.identity()
+                          //   ..scale(newZoom / 100);
                         },
                         width: 40,
                         height: 40,
@@ -149,10 +154,10 @@ class _InteractiveVSNodeViewBodyState
                     const SizedBox(height: 10),
                     CustomButton(
                         onTap: () {
-                          double newZoom = (state.zoomLevel - 5).clamp(5, 500);
-                          context.read<GridCubit>().updateZoom(newZoom);
-                          _transformationController.value = Matrix4.identity()
-                            ..scale(newZoom / 100);
+                          // double newZoom = (state.zoomLevel - 5).clamp(5, 500);
+                          // context.read<GridCubit>().updateZoom(newZoom);
+                          // _transformationController.value = Matrix4.identity()
+                          //   ..scale(newZoom / 100);
                         },
                         width: 40,
                         height: 40,
