196ff66d6da8eb0aa2b2a350430576559ed09814
MoNafea01 <monafe301@gmail.com>
2025-03-07 16:18:00 +0200

    added new line for description of component table for a better view

--- project/core/nodes/configs/preprocessors.py
+++ project/core/nodes/configs/preprocessors.py
@@ -1,15 +1,32 @@
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, Normalizer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder, LabelBinarizer
-from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import (
+    StandardScaler, 
+    MinMaxScaler, 
+    MaxAbsScaler, 
+    RobustScaler, 
+    Normalizer,
+    )
+
+from sklearn.preprocessing import (
+    LabelBinarizer,
+    OrdinalEncoder, 
+    OneHotEncoder, 
+    LabelEncoder, 
+    )
+
+from sklearn.impute import (
+    SimpleImputer, 
+    KNNImputer
+    )
+
 from sklearn.preprocessing import Binarizer
 
 PREPROCESSORS = {
     'scaler':{
-        'standard_scaler':{'node': StandardScaler, 'params': {'with_mean': True, 'with_std': True}},
-        'minmax_scaler':{'node': MinMaxScaler, 'params': {'feature_range': (0, 1)}},
         'maxabs_scaler':{'node': MaxAbsScaler, 'params': {}},
-        'robust_scaler':{'node': RobustScaler, 'params': {'quantile_range': (25.0, 75.0)}},
         'normalizer':{'node': Normalizer, 'params': {'norm': 'l2'}},
+        'minmax_scaler':{'node': MinMaxScaler, 'params': {'feature_range': (0, 1)}},
+        'robust_scaler':{'node': RobustScaler, 'params': {'quantile_range': (25.0, 75.0)}},
+        'standard_scaler':{'node': StandardScaler, 'params': {'with_mean': True, 'with_std': True}},
     },
     'encoder':{
         'label_encoder':{'node': LabelEncoder, 'params':{}},
@@ -18,8 +35,8 @@ PREPROCESSORS = {
         'label_binarizer':{'node': LabelBinarizer, 'params':{}},
     },
     'imputer':{
-        'simple_imputer':{'node': SimpleImputer, 'params':{}},
         'knn_imputer':{'node': KNNImputer, 'params':{}},
+        'simple_imputer':{'node': SimpleImputer, 'params':{}},
     },
     'binarizer':{
         'binarizer':{'node': Binarizer, 'params':{}},
