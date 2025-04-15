3c8302d3f59eec34daf97a160e7426459d06dd79
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-04-16 00:58:07 +0200

    add four icons image path

--- ai_gen/lib/core/themes/asset_paths.dart
+++ ai_gen/lib/core/themes/asset_paths.dart
@@ -1,3 +1,10 @@
 abstract class AssetsPaths {
+  String baseAseetPath="assets/images";
   static const String arrowSelector = "assets/images/arrow_Selector_Tool.png";
+   static const String appleLogo = "assets/images/apple_logo.png";
+   static const String googleLogo = "assets/images/google_logo.png";
+   static const String facebookLogo = "assets/images/facebook_logo.png";
+   static const String githubLogo = "assets/images/github_logo.png";
+
 }
+
