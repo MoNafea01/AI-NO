3f6ef868e1f82d4447c779b3de65e1d40cc6f5fd
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-06 18:11:17 +0300

    add custum widget that handles profile screen

---
+++ ai_gen/lib/features/HomeScreen/widgets/build_profile_info.dart
@@ -0,0 +1,34 @@
+import 'package:ai_gen/core/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+Widget buildInfoTile(BuildContext context, String label, String value) {
+  return Container(
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.grey.shade100,
+      borderRadius: BorderRadius.circular(8),
+      border: Border.all(
+        color: const Color.fromARGB(255, 0, 0, 0),
+        width: 1,
+      ),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: AppColors.primaryColor,
+              ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          value,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ],
+    ),
+  );
+}
