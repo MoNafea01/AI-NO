a644dfdf92b3d17b983126a6c082b27404b0fdb9
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-07-08 15:40:52 +0300

    add debugging to assets paths

--- ai_gen/lib/core/utils/themes/asset_paths.dart
+++ ai_gen/lib/core/utils/themes/asset_paths.dart
@@ -3,57 +3,211 @@ abstract class AssetsPaths {
   static final String arrowSelector = "${baseAssetPath}arrow_Selector_Tool.png";
   static const String refreshIcon = "${baseAssetPath}refresh.svg";
 
-   static const String appleLogo = "${baseAssetPath}apple_logo.png";
-   static const String googleLogo = "${baseAssetPath}google_logo.png";
-   static const String facebookLogo = "${baseAssetPath}facebook_logo.png";
-   static const String githubLogo = "${baseAssetPath}github_logo.png";
+  static const String appleLogo = "${baseAssetPath}apple_logo.png";
+  static const String googleLogo = "${baseAssetPath}google_logo.png";
+  static const String facebookLogo = "${baseAssetPath}facebook_logo.png";
+  static const String githubLogo = "${baseAssetPath}github_logo.png";
 
-
-   // dashboard icons
-   
-    static const String dataSetsIcon = "${baseAssetPath}data_set.png";
-    static const String docsIcon = "${baseAssetPath}docs_sidebar.png";
-    //static const String dashboardIcon = "${baseAssetPath}expanded_icon.svg";
-    static const String exploreIcon = "${baseAssetPath}explore.png";
-    //static const String learnIcon = "${baseAssetPath}learn.svg";
-    static const String modelsBackground = "${baseAssetPath}models_background_icon.png";
- static const String datasetBackground = "${baseAssetPath}dataset_background.png";
+  // dashboard icons
+  static const String dataSetsIcon = "${baseAssetPath}data_set.png";
+  static const String docsIcon = "${baseAssetPath}docs_sidebar.png";
+  //static const String dashboardIcon = "${baseAssetPath}expanded_icon.svg";
+  static const String exploreIcon = "${baseAssetPath}explore.png";
+  //static const String learnIcon = "${baseAssetPath}learn.svg";
+  static const String modelsBackground =
+      "${baseAssetPath}models_background_icon.png";
+  static const String datasetBackground =
+      "${baseAssetPath}dataset_background.png";
   static const String learnBackground = "${baseAssetPath}learn_background.png";
 
-   
-    static const String logOutIcon = "${baseAssetPath}logout.png";
-    static const String notificationIcon = "${baseAssetPath}notifications_icon.svg";
-    static const String modelIcon = "${baseAssetPath}model.png";
-   static const String projectLogoIcon = "${baseAssetPath}project_icon.png";
-    static const String settingIcon = "${baseAssetPath}settings.png";
-    static const String addIcon = "${baseAssetPath}add.png";
-    static const String importIcon = "${baseAssetPath}upload.png";
-     static const String exportIcon = "${baseAssetPath}export.png";
-    static const String collapseIcon = "${baseAssetPath}collapse_expand.png";
-    static const String learnScreenIcon = "${baseAssetPath}learn_sidebar.png";
-    static const String datasetScreenIcon = "${baseAssetPath}dataset_full_icon.png";
+  static const String logOutIcon = "${baseAssetPath}logout.png";
+  static const String notificationIcon =
+      "${baseAssetPath}notifications_icon.svg";
+  static const String modelIcon = "${baseAssetPath}model.png";
+  static const String projectLogoIconPNG = "${baseAssetPath}project_icon.png";
+  static const String projectLogoIcon = "${baseAssetPath}project_icon.svg";
 
-
-
-
-
-  
-
+  static const String settingIcon = "${baseAssetPath}settings.png";
+  static const String addIcon = "${baseAssetPath}add.png";
+  static const String importIcon = "${baseAssetPath}upload.png";
+  static const String exportIcon = "${baseAssetPath}export.png";
+  static const String collapseIcon = "${baseAssetPath}collapse_expand.png";
+  static const String learnScreenIcon = "${baseAssetPath}learn_sidebar.png";
+  static const String datasetScreenIcon =
+      "${baseAssetPath}dataset_full_icon.png";
 
   // dashboard icons
   static const String architectureIcon =
       "${baseAssetPath}architectures_icon.svg";
-  
-  
+
   static const String dashboardIcon = "${baseAssetPath}expanded_icon.svg";
 
   static const String learnIcon = "${baseAssetPath}learn_icon.svg";
-  
-  
-  
- 
+
   static const String expandedIcon = "${baseAssetPath}expanded_icon.svg";
-  
+
   static const String chatBotIcon = "${baseAssetPath}chat_bot.svg";
   static const String chatBotActiveIcon = "${baseAssetPath}chat_bot_active.svg";
 }
+
+///for debugging
+// import 'dart:developer';
+
+// abstract class AssetsPaths {
+//   static const String baseAssetPath = "assets/images/";
+
+//   static String get arrowSelector {
+//     log("arrowSelector: ${baseAssetPath}arrow_Selector_Tool.png");
+//     return "${baseAssetPath}arrow_Selector_Tool.png";
+//   }
+
+//   static String get refreshIcon {
+//     log("refreshIcon: ${baseAssetPath}refresh.svg");
+//     return "${baseAssetPath}refresh.svg";
+//   }
+
+//   static String get appleLogo {
+//     log("appleLogo: ${baseAssetPath}apple_logo.png");
+//     return "${baseAssetPath}apple_logo.png";
+//   }
+
+//   static String get googleLogo {
+//     log("googleLogo: ${baseAssetPath}google_logo.png");
+//     return "${baseAssetPath}google_logo.png";
+//   }
+
+//   static String get facebookLogo {
+//     log("facebookLogo: ${baseAssetPath}facebook_logo.png");
+//     return "${baseAssetPath}facebook_logo.png";
+//   }
+
+//   static String get githubLogo {
+//     log("githubLogo: ${baseAssetPath}github_logo.png");
+//     return "${baseAssetPath}github_logo.png";
+//   }
+
+//   static String get dataSetsIcon {
+//     log("dataSetsIcon: ${baseAssetPath}data_set.png");
+//     return "${baseAssetPath}data_set.png";
+//   }
+
+//   static String get docsIcon {
+//     log("docsIcon: ${baseAssetPath}docs_sidebar.png");
+//     return "${baseAssetPath}docs_sidebar.png";
+//   }
+
+//   static String get exploreIcon {
+//     log("exploreIcon: ${baseAssetPath}explore.png");
+//     return "${baseAssetPath}explore.png";
+//   }
+
+//   static String get modelsBackground {
+//     log("modelsBackground: ${baseAssetPath}models_background_icon.png");
+//     return "${baseAssetPath}models_background_icon.png";
+//   }
+
+//   static String get datasetBackground {
+//     log("datasetBackground: ${baseAssetPath}dataset_background.png");
+//     return "${baseAssetPath}dataset_background.png";
+//   }
+
+//   static String get learnBackground {
+//     log("learnBackground: ${baseAssetPath}learn_background.png");
+//     return "${baseAssetPath}learn_background.png";
+//   }
+
+//   static String get logOutIcon {
+//     log("logOutIcon: ${baseAssetPath}logout.png");
+//     return "${baseAssetPath}logout.png";
+//   }
+
+//   static String get modelIcon {
+//     log("modelIcon: ${baseAssetPath}model.png");
+//     return "${baseAssetPath}model.png";
+//   }
+
+//   static String get settingIcon {
+//     log("settingIcon: ${baseAssetPath}settings.png");
+//     return "${baseAssetPath}settings.png";
+//   }
+
+//   static String get addIcon {
+//     log("addIcon: ${baseAssetPath}add.png");
+//     return "${baseAssetPath}add.png";
+//   }
+
+//   static String get importIcon {
+//     log("importIcon: ${baseAssetPath}upload.png");
+//     return "${baseAssetPath}upload.png";
+//   }
+
+//   static String get importSVG {
+//     log("importSVG: ${baseAssetPath}upload.svg");
+//     return "${baseAssetPath}upload.svg";
+//   }
+
+//   static String get exportIcon {
+//     log("exportIcon: ${baseAssetPath}export.png");
+//     return "${baseAssetPath}export.png";
+//   }
+
+//   static String get collapseIcon {
+//     log("collapseIcon: ${baseAssetPath}collapse_expand.png");
+//     return "${baseAssetPath}collapse_expand.png";
+//   }
+
+//   static String get learnScreenIcon {
+//     log("learnScreenIcon: ${baseAssetPath}learn_sidebar.png");
+//     return "${baseAssetPath}learn_sidebar.png";
+//   }
+
+//   static String get datasetScreenIcon {
+//     log("datasetScreenIcon: ${baseAssetPath}dataset_full_icon.png");
+//     return "${baseAssetPath}dataset_full_icon.png";
+//   }
+
+//   static String get projectLogoIconPNG {
+//     log("projectLogoIconSVG: ${baseAssetPath}project_icon.png");
+//     return "${baseAssetPath}project_icon.png";
+//   }
+
+//   static String get projectLogoIcon {
+//     log("projectLogoIcon: ${baseAssetPath}project_icon.svg");
+//     return "${baseAssetPath}project_icon.svg";
+//   }
+
+//   static String get notificationIcon {
+//     log("notificationIcon: ${baseAssetPath}notifications_icon.svg");
+//     return "${baseAssetPath}notifications_icon.svg";
+//   }
+
+//   static String get architectureIcon {
+//     log("architectureIcon: ${baseAssetPath}architectures_icon.svg");
+//     return "${baseAssetPath}architectures_icon.svg";
+//   }
+
+//   static String get dashboardIcon {
+//     log("dashboardIcon: ${baseAssetPath}expanded_icon.svg");
+//     return "${baseAssetPath}expanded_icon.svg";
+//   }
+
+//   static String get learnIcon {
+//     log("learnIcon: ${baseAssetPath}learn_icon.svg");
+//     return "${baseAssetPath}learn_icon.svg";
+//   }
+
+//   static String get expandedIcon {
+//     log("expandedIcon: ${baseAssetPath}expanded_icon.svg");
+//     return "${baseAssetPath}expanded_icon.svg";
+//   }
+
+//   static String get chatBotIcon {
+//     log("chatBotIcon: ${baseAssetPath}chat_bot.svg");
+//     return "${baseAssetPath}chat_bot.svg";
+//   }
+
+//   static String get chatBotActiveIcon {
+//     log("chatBotActiveIcon: ${baseAssetPath}chat_bot_active.svg");
+//     return "${baseAssetPath}chat_bot_active.svg";
+//   }
+// }
