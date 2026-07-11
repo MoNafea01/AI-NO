f36b90fae1b7a9d3714821276aa2ad158e6cdc3d
MoNafea01 <monafe301@gmail.com>
2026-07-11 12:08:36 +0300

    Removed old Project Structure (Pending new structure using Fast API)

--- project/core/nodes/__init__.py
+++
@@ -1,5 +0,0 @@
-from core.nodes.preprocessing import *
-from core.nodes.other import *
-from core.nodes.model import *
-from core.nodes.nets import *
-from core.nodes.configs.const_ import SAVING_DIR, MULTI_CHANNEL_NODES, DICT_NODES, DATA_NODES, PARENT_NODES
--- project/core/repositories/operations/__init__.py
+++
@@ -1,4 +0,0 @@
-from core.repositories.operations.save import NodeSaver
-from core.repositories.operations.load import NodeLoader
-from core.repositories.operations.delete import NodeDeleter
-from core.repositories.operations.update import NodeUpdater
